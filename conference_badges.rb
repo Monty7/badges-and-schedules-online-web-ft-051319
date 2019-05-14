30bf981bd69679ae8fbfaea61696f72f08e4e9c5
voluble-bug-0219 <montensia1@outlook.com>
2019-05-14 19:20:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,9 +10,11 @@ def batch_badge_creator(names_array)
 end
 
 def assign_rooms(names_array)
-names_array.each_with_index {|name, index|
-    "Hello, #{name}! You'll be assigned to room #{index}"
+  assigned_rooms = []
+  names_array.each_with_index {|name, index|
+    assigned_rooms << "Hello, #{name}! You'll be assigned to room #{index}"
   }
+  return assigned_rooms
 end
 
 def printer
