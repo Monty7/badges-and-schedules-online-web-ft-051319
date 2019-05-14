3ebabe66afc141d0294b2e922ff49cf28a8e9f69
voluble-bug-0219 <montensia1@outlook.com>
2019-05-14 18:58:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,10 @@
 # Write your code here.
-def badge_maker
-  
+def badge_maker(name)
+  return "Hello, my name is #{name}"
 end
 
 def batch_badge_creator(names_array)
+  
   #names_array.collect
 end
 
